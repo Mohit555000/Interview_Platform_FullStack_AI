afa0f869894da92d5e8ca244bc29f6c1e58a25da
Mohit <gangwanimohit42@gmail.com>
2026-03-28 01:56:11 +0530

    added vercel url in CORS, addedd render.yml file and Aptfile in backend

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app = FastAPI(title="InterviewAI API", version="2.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000","https://interview-platform-full-stack-ai-u3.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
